9e1526233a95b1dc07f0c05cb8dad93803687c4c
LADOUE Fabrice <Fabrice.LADOUE@taores.fr>
2017-04-19 11:45:03 +0200

    prem

---
+++ src/main/java/com/lmg/TestSqsApplication.java
@@ -0,0 +1,12 @@
+package com.lmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSqsApplication.class, args);
+	}
+}
